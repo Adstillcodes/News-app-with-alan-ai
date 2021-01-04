92c9147aec677b2a1a67488761f062a64429e389
Anvit Deshpande <70889070+Anvit3@users.noreply.github.com>
2021-01-04 12:53:06 +0530

    first commit

---
+++ build/precache-manifest.428c128bc963e05581359afe3ef68236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312330e57e5a12e4cac758370dc4660f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "145f39d664cabf1a3ecb",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "78f7bacd4ad77c7cc761",
+    "url": "/static/js/2.dd8efa81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dd8efa81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145f39d664cabf1a3ecb",
+    "url": "/static/js/main.b58bc7d5.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
